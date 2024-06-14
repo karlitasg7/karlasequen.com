c47471563b1dd2ca00bb1fd5eeacec032509194d
Karla Sequen <karlasequen@gmail.com>
2024-06-13 22:15:41 -0600

    first commit

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+export default {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: true,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
-import icon from "astro-icon";
-import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), compress()],
-  output: "server",
-  adapter: vercel()
+  integrations: [tailwind(), robotsTxt()],
+  site: "https://karlasequen.com/",
 });
